7078fe187778f82718815a00cfa648923480203c
Christopher Lamm <christopherlamm81@icloud.com>
2025-09-20 22:25:59 -0400

    add application model

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
 package main
 
-func main() {}
+func main() {
+	// model stores the application's state
+	type model struct {
+		choices  []string         // items on the shopping list
+		cursor   int              // which list item the cursor is pointing at
+		selected map[int]struct{} // which list items are selected
+	}
+}
